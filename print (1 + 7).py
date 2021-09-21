62bc1d4a0780b6528f8e6fc62e525baf3c5ad0cf
artiamus <90566909+artiamus@users.noreply.github.com>
2021-09-21 12:29:54 +0300

    Update print (1 + 7).py

--- print (1 + 7).py
+++ print (1 + 7).py
@@ -1,3 +1,15 @@
-print (1 / 7)
+num1 = 5
+num2 = 3
+sum = num1 + num2
+print (str(num1) + "+" + str(num2) + "=" + str(sum))
 
-print(2+2)
+num3 = int(input ())
+num4 = int(input ())
+sum2 = num3 + num4
+print (str(num3) + "+" + str(num4) + "=" + str(sum2))
+number1 = int(input ("pirmais skaitlis"))
+answer = input ("izvelies a=saskaitit b=atnemt c=reizinat d=dalit")
+number2 = int(input("otrais cipars"))
+answer2 = number1 + answer + number2
+print (str(number1) + (answer) + str(number2) + "=" + str(answer2))
+
