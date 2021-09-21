e442933e901baef4100ded830ecf2dbac7b0b12f
artiamus <90566909+artiamus@users.noreply.github.com>
2021-09-21 12:50:13 +0300

    Add files via upload

---
+++ my_list = ['teksts' , 1 , 2].py
@@ -0,0 +1,15 @@
+my_list = ['teksts' , 1 , 2]
+print (my_list)
+print (my_list [2])
+my_list[0] = 'hi!'
+print (my_list [0])
+print (my_list [0:])
+my_list[1] = input ([])
+print (my_list[1])
+my_list[2] = input ([])
+print (my_list[2])
+print (my_list [1:])
+my_list = my_list + ["sdasdas"]
+print (my_list)
+my_list.append ('hello!')
+print (my_list)
