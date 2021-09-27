bcfe4fd417c7513f23375e94773e559787cdbcb2
artiamus <90566909+artiamus@users.noreply.github.com>
2021-09-27 12:44:49 +0300

    Add files via upload

---
+++ aaa = bbb.py
@@ -0,0 +1,21 @@
+aaa = "bbb"
+ccc = "ddd"
+print (aaa)
+print (ccc)
+print (aaa + ccc)
+aaaa = 12345
+bbbb = 123.45
+print (aaaa)
+print (bbbb)
+cccc = aaaa + bbbb
+print (str(aaaa) + "+" + str(bbbb) + "=" + str(cccc))
+print ("{}".format(str))
+aaaaa = [aaa, ccc, aaaa, bbbb, cccc ]
+print (aaaaa)
+print (len(aaaaa))
+aaaaa = [aaa, ccc, aaaa, bbbb, cccc, 123]
+print (aaaaa)
+aaaaa[0] = "asd"
+aaaaa[1] = "abc"
+aaaaa[2] = "wasd"
+print (aaaaa)
