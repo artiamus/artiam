545e432681b7a7dc41f960472d900d3476f312b3
artiamus <90566909+artiamus@users.noreply.github.com>
2021-09-28 12:56:09 +0300

    Add files via upload

---
+++ artjoms tutins 10 a.py
@@ -0,0 +1,20 @@
+#artjoms tutins 10 a
+# 9/28/2021
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for a[0] in a:
+    print (a[0])
+print(17%5)
+for a[0] in a:
+    if a[0]%2 == 0:
+        print(a[0])
+for a[0] in a:
+    if a[0]%2 == 0:
+        print(a[0])
+    else:
+        print("jsfkasndkjas")
+b = ['abc', 'aaaaa', 'bbbbb']
+for b[0] in b:
+    print ("teksts kurs iet pirms", b[0:2])
+for a[0] in range (1, 10, 2):
+    print (a[0])
