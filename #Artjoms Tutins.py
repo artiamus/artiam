68ac05b38cbfee10a034535b9f3fd91590d195d3
artiamus <90566909+artiamus@users.noreply.github.com>
2021-10-04 12:36:14 +0300

    Add files via upload

---
+++ #Artjoms Tutins.py
@@ -0,0 +1,20 @@
+#Artjoms Tutins
+#10/4/2021
+a = 15
+c = 14
+if a == 15:
+    print (1)
+elif a == c:
+    print (2)
+else:
+    print (3)
+a = input()
+print (a)
+b = input ()
+c = input ()
+if b == c:
+    print (1)
+elif b == a:
+    print (4)
+else:
+    print (3)
