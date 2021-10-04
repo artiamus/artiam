2be4859d91ee9be87263bacd669439e1a0f6a355
artiamus <90566909+artiamus@users.noreply.github.com>
2021-10-04 12:49:48 +0300

    Add files via upload

---
+++ #artjoms tutins 10.a.py
@@ -0,0 +1,16 @@
+#artjoms tutins 10.a
+#10/4/2021
+def abc():
+    print ("abc")
+def abc(input):
+    print ("abc", input)
+abc (print)
+def bbb (input, print):
+    print ("sadasdas", input, print)
+bbb (input, print)
+def aaa (aaaa, bbbb):
+    return aaaa + bbbb
+aaaa = 5
+bbbb = 6
+print (aaa(aaaa, bbbb))
+
