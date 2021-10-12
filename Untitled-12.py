58adb3e042ce5792b1cb756c349c532ec25331a0
artiamus <90566909+artiamus@users.noreply.github.com>
2021-10-12 13:54:46 +0300

    Add files via upload

---
+++ Untitled-12.py
@@ -0,0 +1,10 @@
+def skaitli(a, b):
+    if a + b == 20:
+        return True
+    elif a == 20:
+        return True
+    elif b == 20:
+        return True
+    else:
+        return False
+print (skaitli(5, 20))
