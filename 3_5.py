a92a2c6ef31f631baac95f87abb7caf74a60ac6f
artiamus <90566909+artiamus@users.noreply.github.com>
2022-05-03 12:28:26 +0300

    Add files via upload

---
+++ 3_5.py
@@ -0,0 +1,17 @@
+stop = False
+while stop == False:
+    laiks = int(input("ievadi laiku(0-23): "))
+    if laiks >= 0 and laiks <= 23:
+        stop = True
+    else:
+        print("pameigini velreiz")
+if laiks >= 0 and laiks <= 4:
+    print("labunakti!")
+elif laiks >= 5 and laiks <=11:
+    print("labrit!")
+elif laiks >= 12 and laiks <= 17:
+    print("labdien!")
+elif laiks >= 18 and laiks <=23:
+    print("labvakar!")
+else:
+    print("ir kluda!")
